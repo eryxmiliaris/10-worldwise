a23be6f89f8076c149f949757ae96152edaa1e4b
eryxmiliaris <eryxmiliaris232@protonmail.com>
2023-08-27 20:19:57 +0200

    Add list of countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -6,7 +7,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -45,7 +46,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>List of countires</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="Form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,33 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Message from "./Message";
+import Spinner from "./Spinner";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message
+        message={"Add your first city by clicking on a city on the map!"}
+      />
+    );
+
+  const countries = cities.reduce(
+    (arr, city) =>
+      arr.map((el) => el.country).includes(city.country)
+        ? arr
+        : [...arr, { country: city.country, emoji: city.emoji }],
+    []
+  );
+
+  return (
+    <div className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </div>
+  );
+}
+
+export default CountryList;
